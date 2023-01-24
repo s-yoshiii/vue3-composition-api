41b987b2c37456af801d48f263168525c3894007
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-24 11:34:09 +0900

    Vue3でボタンのクリックに反応するイベント定義

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from "@vue/reactivity";
 import { computed } from "@vue/runtime-core";
-
+// Vueの新しい開発手法
 const answer = computed(() => {
   return 1 + 1;
 });
@@ -13,15 +13,27 @@ const message = computed(() => {
 });
 const welcomeColor = "blue";
 const name = ref("");
+
+// Vue3でボタンのクリックに反応するイベント定義
+const number = ref(0);
+const add = () => {
+  number.value++;
+};
 </script>
 <template>
   <h1>Vue 3</h1>
+  <!-- Vueの新しい開発手法 -->
+  <h2>Vueの新しい開発手法</h2>
   <p>1 + 1 = {{ answer }}</p>
   <p>お名前は？：<input type="text" size="30" v-model="name" /></p>
   <p
     v-html="message"
     :style="{ backgroundColor: welcomeColor, color: 'white' }"
   ></p>
+  <!-- Vue3でボタンのクリックに反応するイベント定義 -->
+  <h2>Vue3でボタンのクリックに反応するイベント定義</h2>
+  <input type="text" size="30" v-model="number" />
+  <button @click="add()">+1</button>
 </template>
 
 <style></style>
