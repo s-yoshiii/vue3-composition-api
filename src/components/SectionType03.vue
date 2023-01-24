e4548909efcf0dd59c729afd7794a5ed78ab1c34
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-24 12:12:17 +0900

    条件によって要素を表示・非表示しよう - v-if / v-else

--- src/components/SectionType03.vue
+++ src/components/SectionType03.vue
@@ -6,12 +6,24 @@ const addTodo = () => {
   todos.value.push(newTodo.value);
   newTodo.value = "";
 };
+const removeTodo = (index) => {
+  todos.value.splice(index, 1);
+};
 </script>
 <template>
-  <h2>繰り返し処理でリストを作ろうv-for</h2>
+  <h2>
+    繰り返し処理でリストを作ろう - v-for<br />
+    条件によって要素を表示・非表示しよう - v-if / v-else
+  </h2>
   <input type="text" size="30" v-model="newTodo" />
   <button type="button" @click="addTodo()">追加</button>
-  <ul>
-    <li v-for="(todo, i) in todos" :key="i">{{ i }}. {{ todo }}</li>
+  <ul v-if="todos.length > 0">
+    <li v-for="(todo, i) in todos" :key="i">
+      {{ todo }}
+      <button type="button" @click="removeTodo(i)" style="cursor: pointer">
+        削除
+      </button>
+    </li>
   </ul>
+  <p v-else>※ToDoを追加してください</p>
 </template>
