a897d5ac50ddc12b831679e40d2bf437e97a425c
sa_yoshiii <sayako.yoshikawa1129@gmail.com>
2023-01-24 12:02:33 +0900

    繰り返し処理でリストを作ろうv-for

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import SectionType01 from "@/components/SectionType01.vue";
 import SectionType02 from "@/components/SectionType02.vue";
+import SectionType03 from "@/components/SectionType03.vue";
 </script>
 <template>
   <h1>Vue 3</h1>
   <SectionType01 />
   <SectionType02 />
+  <SectionType03 />
 </template>
 
 <style></style>
---
+++ src/components/SectionType03.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from "vue";
+const newTodo = ref("");
+const todos = ref([]);
+const addTodo = () => {
+  todos.value.push(newTodo.value);
+  newTodo.value = "";
+};
+</script>
+<template>
+  <h2>繰り返し処理でリストを作ろうv-for</h2>
+  <input type="text" size="30" v-model="newTodo" />
+  <button type="button" @click="addTodo()">追加</button>
+  <ul>
+    <li v-for="(todo, i) in todos" :key="i">{{ i }}. {{ todo }}</li>
+  </ul>
+</template>
